36c1ddbb32b9c75a319e47b06430b3465cf378fe
aneelia <aneelia@bk.ru>
2019-12-21 15:45:43 +0300

    feat(patterns): implement hoc simple example

---
+++ src/patterns/hoc/simple-hoc.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+class SharedComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+const enhancer = (Component) => {
+    return class Enhancer extends React.Component {
+        render() {
+            return (
+                <Component>
+                    <h1>My HOC</h1>
+                </Component>
+            );
+        }
+    }
+}
+
+const SimpleHOC = enhancer(SharedComponent)
+
+export default SimpleHOC
