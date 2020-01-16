6856f90d0204b564983176fa6c4f9e960a72b2fb
aneelia <aneelia@bk.ru>
2020-01-17 00:47:30 +0300

    fix(redux): delete unused files

--- src/redux/redux-implementation/create-store.js
+++
@@ -1,9 +0,0 @@
-function createStore(reducer, initialState){
-    let state = initialState
-    return {
-        dispatch: action => { state = reducer(state, action) },
-        getState: () => state,
-    }
-}
-
-export default createStore;
